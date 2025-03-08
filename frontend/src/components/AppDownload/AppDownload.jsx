031b17d5b1eb42c04e6da6e55bbe915dc7a90c63
Abhishek17966 <abhishekkumsrsingh17966@gmail.com>
2025-03-08 19:32:50 +0530

    first commit

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./AppDownload.css";
+import { assets } from "../../assets/assets";
+
+const AppDownload = () => {
+  return (
+    <div className="app-download" id="app-download">
+      <p>
+        For Better Experience Download <br /> Onion App
+      </p>
+      <div className="app-download-platforms">
+        <a href="https://play.google.com/store" target="_blank" rel="noopener noreferrer">
+          <img src={assets.play_store} alt="Play Store" />
+        </a>
+        <a href="https://apps.apple.com" target="_blank" rel="noopener noreferrer">
+          <img src={assets.app_store} alt="App Store" />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default AppDownload;
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div id="footer" className="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+        <img src={assets.logo} alt="" style={{ width: "150px", height: "auto" }} />
+          <p>
+          Order from us today and enjoy the best quality Food delivered to your door! For more details, connect with us through our social media.
+          </p>
+          <div className="footer-social-icons">
+            <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+              <img src={assets.facebook_icon} alt="Facebook" />
+            </a>
+            <a href="https://x.com/?lang=en&mx=2" target="_blank" rel="noopener noreferrer">
+              <img src={assets.twitter_icon} alt="Twitter" />
+            </a>
+            <a href="https://www.linkedin.com/in/abhi190902/" target="_blank" rel="noopener noreferrer">
+              <img src={assets.linkedin_icon} alt="LinkedIn" />
+            </a>
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>Company</h2>
+          <ul>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>Get in Touch</h2>
+          <ul>
+            <li>+91 8709252439</li>
+            <li>inet.abhisheksingh@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 © Onion.com - All Rights Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
